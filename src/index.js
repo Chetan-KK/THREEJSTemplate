1f5b19e035a91dfed3d21da25a61ac4155b107cf
Chetan <chetankhulage350@gmail.com>
2022-06-24 13:08:14 +0530

    pages updated

--- src/index.js
+++ src/index.js
@@ -1,61 +1,59 @@
-import './style/main.css'
-import * as THREE from 'three'
+import './style/main.css';
+import * as THREE from 'three';
 
 /**
  * Sizes
  */
-const sizes = {}
-sizes.width = window.innerWidth
-sizes.height = window.innerHeight
+const sizes = {};
+sizes.width = window.innerWidth;
+sizes.height = window.innerHeight;
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Save sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
 
     // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
 
     // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-})
+    renderer.setSize(sizes.width, sizes.height);
+});
 
 /**
  * Environnements
  */
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.z = 3;
+scene.add(camera);
 
 // Test
-const cube = new THREE.Mesh(new THREE.BoxBufferGeometry(1, 1, 1), new THREE.MeshNormalMaterial())
-scene.add(cube)
+const cube = new THREE.Mesh(new THREE.BoxBufferGeometry(1, 1, 1), new THREE.MeshNormalMaterial());
+scene.add(cube);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('.webgl')
-})
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(sizes.width, sizes.height)
+    canvas: document.querySelector('.firstCanvas')
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(sizes.width, sizes.height);
 
 /**
  * Loop
  */
-const loop = () =>
-{
+const loop = () => {
     // Update
-    cube.rotation.y += 0.01
+    cube.rotation.y += 0.01;
 
     // Render
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
 
     // Keep looping
-    window.requestAnimationFrame(loop)
-}
-loop()
+    window.requestAnimationFrame(loop);
+};
+loop();
